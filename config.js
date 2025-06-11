d3cab19c9195491c1cac3285c70550aff53505ed
Markin <138695926+Marcos-M1randa@users.noreply.github.com>
2025-06-11 00:06:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dspi4f76w/video/upload/v1749609551/Marcos_Belutti_-_Sonho_De_Todo_Casal_Energia_ry4emq.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
